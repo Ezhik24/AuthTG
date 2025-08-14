2352aecc311934d31644c1c1c5f1a1786d7a8f77
Ezhik24 <nikita_melnikov_2024@mail.ru>
2025-08-14 11:34:13 +0300

    update KickCMD in tg

--- src/main/java/org/ezhik/authTG/commandTG/KickCMDHandler.java
+++ src/main/java/org/ezhik/authTG/commandTG/KickCMDHandler.java
@@ -11,23 +11,25 @@ public class KickCMDHandler implements CommandHandler {
     public void execute(Update update) {
         User user = User.getCurrentUser(update.getMessage().getChatId());
         if (user.activetg) {
-            if (user.isadmin) {
+            if (user.isadmin || user.commands.contains("kick")) {
                 String[] args = update.getMessage().getText().toString().split(" ");
                 if (args.length < 2) {
                     user.sendMessage("Введите никнейм и причину");
                     AuthTG.bot.setNextStepHandler(update.getMessage().getChatId(), new KickAskHandler());
                 } else {
-                    String message = String.join(" ", args).substring(args[0].length() + 1).substring(args[1].length() + 1);
-                    if (message.isEmpty()) {
+                    if (args.length < 3) {
                         user.sendMessage("Команда введена не полностью, введите: /kick <никнейм> <причина>");
                     } else {
+                        String message = String.join(" ", args).substring(args[0].length() + 1).substring(args[1].length() + 1);
                         User user1 = User.getUser(args[1]);
-                        if (user1.player == null) {
-                            user.sendMessage("Игрок не онлайн");
-                        } else {
-                            Handler.kick(user1.playername, message);
-                            user.sendMessage("Игрок " + user1.playername + " был успешно кикнут");
-                        }
+                        if (user1 != null) {
+                            if (user1.player == null) {
+                                user.sendMessage("Игрок не онлайн");
+                            } else {
+                                Handler.kick(user1.playername, message);
+                                user.sendMessage("Игрок " + user1.playername + " был успешно кикнут");
+                            }
+                        } else user.sendMessage("Игрок не авторизован");
                     }
                 }
             } else user.sendMessage("У вас нет прав");
