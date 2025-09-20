57bcd5ea2b0162b052253eafc543a117dc10138b
ezhik <nikita_melnikov_2024@mail.ru>
2025-09-20 13:55:04 +0300

    added messages in config.yml

--- src/main/java/org/ezhik/authTG/commandTG/BanCMDHandler.java
+++ src/main/java/org/ezhik/authTG/commandTG/BanCMDHandler.java
@@ -15,35 +15,35 @@ public class BanCMDHandler implements CommandHandler {
     public void execute(Update update) {
         User user = User.getCurrentUser(update.getMessage().getFrom().getId());
         if (!user.activetg) {
-            AuthTG.bot.sendMessage(update.getMessage().getChatId(), "[Бот] Вы не авторизованы");
+            AuthTG.bot.sendMessage(update.getMessage().getChatId(), AuthTG.config.getString("messages.telegram.bannoactivetg"));
             return;
         }
         if (user.isadmin || user.commands != null && user.commands.contains("ban")) {
             String[] args = update.getMessage().getText().split(" ");
             if (args.length < 2) {
-                user.sendMessage("Напишите: <никнейм> <время> <причина> или <никнейм> <причина>");
+                user.sendMessage(AuthTG.config.getString("messages.telegram.ban"));
                 AuthTG.bot.setNextStepHandler(update.getMessage().getChatId(), new BanAskHandler());
             } else {
                 if (args.length < 4) {
-                    user.sendMessage("Использование: /ban <никнейм> <время> <причина>");
+                    user.sendMessage(AuthTG.config.getString("messages.telegram.banusage"));
                     return;
                 }
                 User user1 = User.getUser(args[1]);
                 if (user1 == null) {
-                    user.sendMessage("Пользователь не авторизован");
+                    user.sendMessage(AuthTG.config.getString("messages.telegram.banusernotfound"));
                     return;
                 }
                 if (AuthTG.loader.isBanned(user1.uuid)) {
-                    user.sendMessage("Пользователь уже забанен");
+                    user.sendMessage(AuthTG.config.getString("messages.telegram.banalreadybanned"));
                     return;
                 }
                 if (args[2].startsWith("0")) {
-                    user.sendMessage("Вы не можете забанить пользователя на 0");
+                    user.sendMessage(AuthTG.config.getString("messages.telegram.bantimenull"));
                     return;
                 }
                 String reason = String.join(" ", args).substring(args[0].length() + args[1].length() + args[2].length() + 3);
                 if (reason.length() > 120) {
-                    user.sendMessage("Слишком длинная причина");
+                    user.sendMessage(AuthTG.config.getString("messages.telegram.banreasonlong"));
                     return;
                 }
                 if (args[2].contains("d")) {
@@ -53,7 +53,7 @@ public class BanCMDHandler implements CommandHandler {
                     String formattedDate = dateTime.format(formatter);
                     String time = date.format(formatter);
                     AuthTG.loader.setBanTime(user1.uuid, formattedDate, reason, time, user.playername);
-                    user.sendMessage("Вы забанили " + user1.playername + " на " + args[2].replace("d", "") + " дней");
+                    user.sendMessage(AuthTG.config.getString("messages.telegram.bansuccess").replace("{PLAYER}", user1.playername));
                     if (user1.player != null) Handler.kick(user1.playername, ChatColor.translateAlternateColorCodes('&', AuthTG.config.getString("messages.minecraft.ban")).replace("{ADMIN}", user.playername).replace("{REASON}", reason).replace("{TIMEBAN}", formattedDate).replace("{TIME}", time).replace("{BR}", "\n"));
                 } else if (args[2].contains("h")) {
                     LocalDateTime date = LocalDateTime.now();
@@ -62,7 +62,7 @@ public class BanCMDHandler implements CommandHandler {
                     String formattedDate = dateTime.format(formatter);
                     String time = date.format(formatter);
                     AuthTG.loader.setBanTime(user1.uuid, formattedDate, reason, time, user.playername);
-                    user.sendMessage("Вы забанили " + user1.playername + " на " + args[2].replace("h", "") + " часов");
+                    user.sendMessage(AuthTG.config.getString("messages.telegram.bansuccess").replace("{PLAYER}", user1.playername));
                     if (user1.player != null) Handler.kick(user1.playername, ChatColor.translateAlternateColorCodes('&', AuthTG.config.getString("messages.minecraft.ban")).replace("{ADMIN}", user.playername).replace("{REASON}", reason).replace("{TIMEBAN}", formattedDate).replace("{TIME}", time).replace("{BR}", "\n"));
                 } else if (args[2].contains("m")) {
                     LocalDateTime date = LocalDateTime.now();
@@ -71,7 +71,7 @@ public class BanCMDHandler implements CommandHandler {
                     String formattedDate = dateTime.format(formatter);
                     String time = date.format(formatter);
                     AuthTG.loader.setBanTime(user1.uuid, formattedDate, reason, time, user.playername);
-                    user.sendMessage("Вы забанили " + user1.playername + " на " + args[2].replace("m", "") + " минут");
+                    user.sendMessage(AuthTG.config.getString("messages.telegram.bansuccess").replace("{PLAYER}", user1.playername));
                     if (user1.player != null) Handler.kick(user1.playername, ChatColor.translateAlternateColorCodes('&', AuthTG.config.getString("messages.minecraft.ban")).replace("{ADMIN}", user.playername).replace("{REASON}", reason).replace("{TIMEBAN}", formattedDate).replace("{TIME}", time).replace("{BR}", "\n"));
                 } else if (args[2].contains("s")) {
                     LocalDateTime date = LocalDateTime.now();
@@ -80,19 +80,19 @@ public class BanCMDHandler implements CommandHandler {
                     String formattedDate = dateTime.format(formatter);
                     String time = date.format(formatter);
                     AuthTG.loader.setBanTime(user1.uuid, formattedDate, reason, time, user.playername);
-                    user.sendMessage("Вы забанили " + user1.playername + " на " + args[2].replace("s", "") + " секунд");
+                    user.sendMessage(AuthTG.config.getString("messages.telegram.bansuccess").replace("{PLAYER}", user1.playername));
                     if (user1.player != null) Handler.kick(user1.playername, ChatColor.translateAlternateColorCodes('&', AuthTG.config.getString("messages.minecraft.ban")).replace("{ADMIN}", user.playername).replace("{REASON}", reason).replace("{TIMEBAN}", formattedDate).replace("{TIME}", time).replace("{BR}", "\n"));
                 } else if (args[2].equals("-s")) {
                     LocalDateTime date = LocalDateTime.now();
                     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss dd.MM.yyyy");
                     String formattedDate = date.format(formatter);
                     AuthTG.loader.setBanTime(user1.uuid, "0", reason, formattedDate, user.playername);
-                    user.sendMessage("Вы забанили " + user1.playername + " на навсегда");
+                    user.sendMessage(AuthTG.config.getString("messages.telegram.bansuccess").replace("{PLAYER}", user1.playername));
                     if (user1.player != null) Handler.kick(user1.playername, ChatColor.translateAlternateColorCodes('&', AuthTG.config.getString("messages.minecraft.ban")).replace("{ADMIN}", user.playername).replace("{REASON}", reason).replace("{TIMEBAN}", "навсегда").replace("{TIME}",formattedDate).replace("{BR}", "\n"));
                 } else {
-                    user.sendMessage("Неверный формат времени (Используйте d, h, m, s или -s)");
+                    user.sendMessage(AuthTG.config.getString("messages.telegram.bantimeformat"));
                 }
             }
-        } else user.sendMessage("У вас недостаточно прав");
+        } else user.sendMessage(AuthTG.config.getString("messages.telegram.bannoperm"));
     }
 }
--- src/main/java/org/ezhik/authTG/nextStep/BanAskHandler.java
+++ src/main/java/org/ezhik/authTG/nextStep/BanAskHandler.java
@@ -18,28 +18,28 @@ public class BanAskHandler implements NextStepHandler {
         User user = User.getCurrentUser(update.getMessage().getChatId());
         String[] args = update.getMessage().getText().split(" ");
         if (args.length < 3) {
-            user.sendMessage("Неверный формат команды");
+            user.sendMessage(AuthTG.config.getString("messages.telegram.banformat"));
             AuthTG.bot.remNextStepHandler(update.getMessage().getChatId());
             return;
         }
         if (user1 == null) {
-            user.sendMessage("Пользователь не авторизован");
+            user.sendMessage(AuthTG.config.getString("messages.telegram.banusernotfound"));
             AuthTG.bot.remNextStepHandler(update.getMessage().getChatId());
             return;
         }
         if (AuthTG.loader.isBanned(user1.uuid)) {
-            user.sendMessage("Пользователь уже забанен");
+            user.sendMessage(AuthTG.config.getString("messages.telegram.banalreadybanned"));
             AuthTG.bot.remNextStepHandler(update.getMessage().getChatId());
             return;
         }
         if (args[1].equals("0")) {
-            user.sendMessage("Вы не можете забанить пользователя на 0");
+            user.sendMessage(AuthTG.config.getString("messages.telegram.bantimenull"));
             AuthTG.bot.remNextStepHandler(update.getMessage().getChatId());
             return;
         }
         String reason = String.join(" ", args).substring(args[0].length() + args[1].length() + 2);
         if (reason.length() > 120) {
-            user.sendMessage("Слишком длинная причина");
+            user.sendMessage(AuthTG.config.getString("messages.telegram.banreasonlong"));
             AuthTG.bot.remNextStepHandler(update.getMessage().getChatId());
             return;
         }
@@ -50,7 +50,7 @@ public class BanAskHandler implements NextStepHandler {
             String formattedDate = dateTime.format(formatter);
             String time = date.format(formatter);
             AuthTG.loader.setBanTime(user1.uuid, formattedDate, reason, time, user.playername);
-            user.sendMessage("Вы забанили " + user1.playername + " на " + args[1] + " дней");
+            user.sendMessage(AuthTG.config.getString("messages.telegram.bansuccess").replace("{PLAYER}", user1.playername));
             if (user1.player != null) Handler.kick(user1.playername, ChatColor.translateAlternateColorCodes('&', AuthTG.config.getString("messages.minecraft.ban")).replace("{ADMIN}", user.playername).replace("{REASON}", reason).replace("{TIMEBAN}", formattedDate).replace("{TIME}", time).replace("{BR}", "\n"));
         } else if (args[1].contains("h")) {
             LocalDateTime date = LocalDateTime.now();
@@ -59,7 +59,7 @@ public class BanAskHandler implements NextStepHandler {
             String formattedDate = dateTime.format(formatter);
             String time = date.format(formatter);
             AuthTG.loader.setBanTime(user1.uuid, formattedDate, reason, time, user.playername);
-            user.sendMessage("Вы забанили " + user1.playername + " на " + args[1] + " часов");
+            user.sendMessage(AuthTG.config.getString("messages.telegram.bansuccess").replace("{PLAYER}", user1.playername));
             if (user1.player != null) Handler.kick(user1.playername, ChatColor.translateAlternateColorCodes('&', AuthTG.config.getString("messages.minecraft.ban")).replace("{ADMIN}", user.playername).replace("{REASON}", reason).replace("{TIMEBAN}", formattedDate).replace("{TIME}", time).replace("{BR}", "\n"));
         } else if (args[1].contains("m")) {
             LocalDateTime date = LocalDateTime.now();
@@ -68,7 +68,7 @@ public class BanAskHandler implements NextStepHandler {
             String formattedDate = dateTime.format(formatter);
             String time = date.format(formatter);
             AuthTG.loader.setBanTime(user1.uuid, formattedDate, reason, time, user.playername);
-            user.sendMessage("Вы забанили " + user1.playername + " на " + args[1] + " минут");
+            user.sendMessage(AuthTG.config.getString("messages.telegram.bansuccess").replace("{PLAYER}", user1.playername));
             if (user1.player != null) Handler.kick(user1.playername, ChatColor.translateAlternateColorCodes('&', AuthTG.config.getString("messages.minecraft.ban")).replace("{ADMIN}", user.playername).replace("{REASON}", reason).replace("{TIMEBAN}", formattedDate).replace("{TIME}", time).replace("{BR}", "\n"));
         } else if (args[1].contains("s")) {
             LocalDateTime date = LocalDateTime.now();
@@ -77,17 +77,17 @@ public class BanAskHandler implements NextStepHandler {
             String formattedDate = dateTime.format(formatter);
             String time = date.format(formatter);
             AuthTG.loader.setBanTime(user1.uuid, formattedDate, reason, time, user.playername);
-            user.sendMessage("Вы забанили " + user1.playername + " на " + args[1] + " секунд");
+            user.sendMessage(AuthTG.config.getString("messages.telegram.bansuccess").replace("{PLAYER}", user1.playername));
             if (user1.player != null) Handler.kick(user1.playername, ChatColor.translateAlternateColorCodes('&', AuthTG.config.getString("messages.minecraft.ban")).replace("{ADMIN}", user.playername).replace("{REASON}", reason).replace("{TIMEBAN}", formattedDate).replace("{TIME}", time).replace("{BR}", "\n"));
         } else if (args[1].equals("-s")) {
             LocalDateTime date = LocalDateTime.now();
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss dd.MM.yyyy");
             String formattedDate = date.format(formatter);
             AuthTG.loader.setBanTime(user1.uuid, "0", reason, formattedDate, user.playername);
-            user.sendMessage("Вы забанили " + user1.playername + " на навсегда");
+            user.sendMessage(AuthTG.config.getString("messages.telegram.bansuccess").replace("{PLAYER}", user1.playername));
             if (user1.player != null) Handler.kick(user1.playername, ChatColor.translateAlternateColorCodes('&', AuthTG.config.getString("messages.minecraft.ban")).replace("{ADMIN}", user.playername).replace("{REASON}", reason).replace("{TIMEBAN}", "навсегда").replace("{TIME}", formattedDate).replace("{BR}", "\n"));
         } else {
-            user.sendMessage("Неверный формат времени (Используйте d, h, m, s)");
+            user.sendMessage(AuthTG.config.getString("messages.telegram.bantimeformat"));
             AuthTG.bot.remNextStepHandler(update.getMessage().getChatId());
             return;
         }
