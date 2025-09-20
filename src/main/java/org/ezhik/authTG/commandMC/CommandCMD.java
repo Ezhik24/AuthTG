0b37c0e8cb084de5ab14393c8085ffd6ec3e3604
ezhik <nikita_melnikov_2024@mail.ru>
2025-09-20 13:10:55 +0300

    added messages in config.yml

--- src/main/java/org/ezhik/authTG/commandMC/CommandCMD.java
+++ src/main/java/org/ezhik/authTG/commandMC/CommandCMD.java
@@ -13,73 +13,74 @@ public class CommandCMD implements CommandExecutor {
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
         Player player = (Player) commandSender;
         if (!AuthTG.loader.isAdmin(player.getUniqueId())) {
-            player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&c&l[AuthTG] &cУ вас нет прав для выполнения этой команды!"));
+            player.sendMessage(ChatColor.translateAlternateColorCodes('&', AuthTG.config.getString("messages.minecraft.cmdnoperm")));
             return false;
         }
         if (strings.length == 0) {
-            player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&c&l[AuthTG] &cКоманда введена неверно! Введите: /command <add | rem | list>"));
+            player.sendMessage(ChatColor.translateAlternateColorCodes('&', AuthTG.config.getString("messages.minecraft.cmdhelp")));
             return false;
         }
         if (strings[0].equals("add")) {
             if (strings.length != 3) {
-                player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&c&l[AuthTG] &cКоманда введена неверно! Введите: /command add <ник> <ban | mute | kick>"));
+                player.sendMessage(ChatColor.translateAlternateColorCodes('&', AuthTG.config.getString("messages.minecraft.cmdhelpadd")));
                 return false;
             }
             if (strings[2].equals("ban") || strings[2].equals("mute") || strings[2].equals("kick")) {
                 User user = User.getUser(strings[1]);
                 if (user == null) {
-                    player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&c&l[AuthTG] &cПользователь не найден!"));
+                    player.sendMessage(ChatColor.translateAlternateColorCodes('&', AuthTG.config.getString("messages.minecraft.cmdusernotfound")));
                     return false;
                 }
                 if (user.commands != null && user.commands.contains(strings[2])) {
-                    player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&c&l[AuthTG] &cКоманда уже добавлена!"));
+                    player.sendMessage(ChatColor.translateAlternateColorCodes('&', AuthTG.config.getString("messages.minecraft.cmdaddalready")));
                     return false;
                 }
                 AuthTG.loader.addCommand(user.uuid, strings[2]);
-                if (user.player != null) user.player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&c&l[AuthTG] &cВам выдали доступ к команде: " + strings[2]));
-                if (user.activetg) user.sendMessage("Вым выдали доступ к команде: " + strings[2]);
-                player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&c&l[AuthTG] &cВы выдали доступ к команде: " + strings[2] + " игроку: " + strings[1]));
-            } else player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&c&l[AuthTG] &cКоманда введена неверно! Введите: /command add <ник> <ban | mute | kick>"));
+                if (user.player != null) user.player.sendMessage(ChatColor.translateAlternateColorCodes('&', AuthTG.config.getString("messages.minecraft.cmdadded").replace("{COMMAND}", strings[2])));
+                if (user.activetg) user.sendMessage(AuthTG.config.getString("messages.telegram.cmdadded").replace("{COMMAND}", strings[2]));
+                player.sendMessage(ChatColor.translateAlternateColorCodes('&', AuthTG.config.getString("messages.minecraft.cmdadd").replace("{COMMAND}", strings[2]).replace("{PLAYER}", strings[1])));
+                } else player.sendMessage(ChatColor.translateAlternateColorCodes('&', AuthTG.config.getString("messages.minecraft.cmdhelpadd")));
         }
         else if (strings[0].equals("rem")) {
             if (strings.length != 3) {
-                player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&c&l[AuthTG] &cКоманда введена неверно! Введите: /command rem <ник> <ban | mute | kick>"));
+                player.sendMessage(ChatColor.translateAlternateColorCodes('&', AuthTG.config.getString("messages.minecraft.cmdhelprem")));
                 return false;
             }
             if (strings[2].equals("ban") || strings[2].equals("mute") || strings[2].equals("kick")) {
                 User user = User.getUser(strings[1]);
                 if (user == null) {
-                    player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&c&l[AuthTG] &cПользователь не найден!"));
+                    player.sendMessage(ChatColor.translateAlternateColorCodes('&', AuthTG.config.getString("messages.minecraft.cmdusernotfound")));
                     return false;
                 }
                 if (user.commands != null && !user.commands.contains(strings[2])) {
-                    player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&c&l[AuthTG] &cКоманда и так не выдана!"));
+                    player.sendMessage(ChatColor.translateAlternateColorCodes('&', AuthTG.config.getString("messages.minecraft.cmdremnot")));
                     return false;
                 }
                 AuthTG.loader.removeCommand(user.uuid, strings[2]);
-                if (user.player != null) user.player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&c&l[AuthTG] &cВам удалена команда: " + strings[2]));
-                if (user.activetg) user.sendMessage("Вым удалена команда: " + strings[2]);
-                player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&c&l[AuthTG] &cВы удалили доступ к команде: " + strings[2] + " игроку: " + strings[1]));
-            } else player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&c&l[AuthTG] &cКоманда введена неверно! Введите: /command rem <ник> <ban | mute | kick>"));
+                if (user.player != null) user.player.sendMessage(ChatColor.translateAlternateColorCodes('&', AuthTG.config.getString("messages.minecraft.cmdrem").replace("{COMMAND}", strings[2])));
+                if (user.activetg) user.sendMessage(AuthTG.config.getString("messages.telegram.cmdrem").replace("{COMMAND}", strings[2]));
+                player.sendMessage(ChatColor.translateAlternateColorCodes('&', AuthTG.config.getString("messages.minecraft.cmdrem").replace("{COMMAND}", strings[2]).replace("{PLAYER}", strings[1])));
+            } else player.sendMessage(ChatColor.translateAlternateColorCodes('&', AuthTG.config.getString("messages.minecraft.cmdhelprem")));
         }
         else if (strings[0].equals("list")) {
             if (strings.length != 2) {
-                player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&c&l[AuthTG] &cКоманда введена неверно! Введите: /command list <ник>"));
+                player.sendMessage(ChatColor.translateAlternateColorCodes('&', AuthTG.config.getString("messages.minecraft.cmdhelplist")));
                 return false;
             }
             User user = User.getUser(strings[1]);
             if (user == null) {
-                player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&c&l[AuthTG] &cПользователь не найден!"));
+                player.sendMessage(ChatColor.translateAlternateColorCodes('&', AuthTG.config.getString("messages.minecraft.cmdusernotfound")));
                 return false;
             }
             if (user.commands != null && user.commands.isEmpty()) {
-                player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&c&l[AuthTG] &cУ игрока: " + strings[1] + " нет доступных команд!"));
+                player.sendMessage(ChatColor.translateAlternateColorCodes('&', AuthTG.config.getString("messages.minecraft.cmdlistempty").replace("{PLAYER}", strings[1])));
                 return false;
             }
-            player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&c&l[AuthTG] &cДоступные команды для игрока: " + strings[1] + ": " + user.commands.toString().replace("[", "").replace("]", "")));
+            String commands = user.commands.toString().replace("[", "").replace("]", "");
+            player.sendMessage(ChatColor.translateAlternateColorCodes('&', AuthTG.config.getString("messages.minecraft.cmdlist").replace("{PLAYER}", strings[1]).replace("{COMMANDS}", commands)));
         }
         else {
-            player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&c&l[AuthTG] &cКоманда введена неверно! Введите: /command <add | rem | list>"));
+            player.sendMessage(ChatColor.translateAlternateColorCodes('&', AuthTG.config.getString("messages.minecraft.cmdhelp")));
             return false;
         }
         return true;
