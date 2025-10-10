951b3a65366d0ede01f9ea0d3828dc872de5e8d7
ezhik <nikita_melnikov_2024@mail.ru>
2025-10-10 17:09:59 +0300

    update mute nextStepHandler

--- src/main/java/org/ezhik/authTG/nextStep/MuteAskHandler.java
+++ src/main/java/org/ezhik/authTG/nextStep/MuteAskHandler.java
@@ -17,28 +17,28 @@ public class MuteAskHandler implements NextStepHandler {
         User target = User.getUser(playername);
         String[] args = update.getMessage().getText().split(" ");
         if (args.length < 3) {
-            user.sendMessage(AuthTG.config.getString("messages.minecraft.muteformat"));
+            user.sendMessage(AuthTG.config.getString("messages.telegram.muteformat"));
             AuthTG.bot.remNextStepHandler(update.getMessage().getChatId());
             return;
         }
         if (target == null) {
-            user.sendMessage(AuthTG.config.getString("messages.minecraft.muteplayernotfound"));
+            user.sendMessage(AuthTG.config.getString("messages.telegram.muteplayernotfound"));
             AuthTG.bot.remNextStepHandler(update.getMessage().getChatId());
             return;
         }
         if (AuthTG.loader.isMuted(target.uuid)) {
-            user.sendMessage(AuthTG.config.getString("messages.minecraft.mutealready"));
+            user.sendMessage(AuthTG.config.getString("messages.telegram.mutealready"));
             AuthTG.bot.remNextStepHandler(update.getMessage().getChatId());
             return;
         }
         if (args[1].equals("0")) {
-            user.sendMessage(AuthTG.config.getString("messages.minecraft.mutetimezero"));
+            user.sendMessage(AuthTG.config.getString("messages.telegram.mutetimezero"));
             AuthTG.bot.remNextStepHandler(update.getMessage().getChatId());
             return;
         }
         String reason = String.join(" ", args).substring(args[0].length() + args[1].length() + 2);
         if (reason.length() > 120) {
-            user.sendMessage(AuthTG.config.getString("messages.minecraft.mutereasonlong"));
+            user.sendMessage(AuthTG.config.getString("messages.telegram.mutereasonlong"));
             AuthTG.bot.remNextStepHandler(update.getMessage().getChatId());
             return;
         }
@@ -49,7 +49,7 @@ public class MuteAskHandler implements NextStepHandler {
             String formattedDate = date.format(formatter);
             String time = timedate.format(formatter);
             AuthTG.loader.setMuteTime(target.uuid, formattedDate, reason, time, user.playername);
-            user.sendMessage(AuthTG.config.getString("messages.minecraft.mutesuccess").replace("{PLAYER}", target.playername));
+            user.sendMessage(AuthTG.config.getString("messages.telegram.mutesuccess").replace("{PLAYER}", target.playername));
             String message = ChatColor.translateAlternateColorCodes('&', AuthTG.config.getString("messages.minecraft.mute")).replace("{TIMEMUTE}", AuthTG.loader.getMuteTime(target.uuid)).replace("{REASON}", AuthTG.loader.getMuteReason(target.uuid)).replace("{TIME}", AuthTG.loader.getMuteTimeAdmin(target.uuid)).replace("{ADMIN}", AuthTG.loader.getMuteAdmin(target.uuid)).replace("{BR}", "\n");
             if (target.player != null) target.player.sendMessage(message);
         } else if (args[1].contains("h")) {
@@ -59,7 +59,7 @@ public class MuteAskHandler implements NextStepHandler {
             String formattedDate = date.format(formatter);
             String time = timedate.format(formatter);
             AuthTG.loader.setMuteTime(target.uuid, formattedDate, reason, time, user.playername);
-            user.sendMessage(AuthTG.config.getString("messages.minecraft.mutesuccess").replace("{PLAYER}", target.playername));
+            user.sendMessage(AuthTG.config.getString("messages.telegram.mutesuccess").replace("{PLAYER}", target.playername));
             String message = ChatColor.translateAlternateColorCodes('&', AuthTG.config.getString("messages.minecraft.mute")).replace("{TIMEMUTE}", AuthTG.loader.getMuteTime(target.uuid)).replace("{REASON}", AuthTG.loader.getMuteReason(target.uuid)).replace("{TIME}", AuthTG.loader.getMuteTimeAdmin(target.uuid)).replace("{ADMIN}", AuthTG.loader.getMuteAdmin(target.uuid)).replace("{BR}", "\n");
             if (target.player != null) target.player.sendMessage(message);
         } else if (args[1].contains("m")) {
@@ -71,7 +71,7 @@ public class MuteAskHandler implements NextStepHandler {
             AuthTG.loader.setMuteTime(target.uuid, formattedDate, reason, time, user.playername);
             String message = ChatColor.translateAlternateColorCodes('&', AuthTG.config.getString("messages.minecraft.mute")).replace("{TIMEMUTE}", AuthTG.loader.getMuteTime(target.uuid)).replace("{REASON}", AuthTG.loader.getMuteReason(target.uuid)).replace("{TIME}", AuthTG.loader.getMuteTimeAdmin(target.uuid)).replace("{ADMIN}", AuthTG.loader.getMuteAdmin(target.uuid)).replace("{BR}", "\n");
             if (target.player != null) target.player.sendMessage(message);
-            user.sendMessage(AuthTG.config.getString("messages.minecraft.mutesuccess").replace("{PLAYER}", target.playername));
+            user.sendMessage(AuthTG.config.getString("messages.telegram.mutesuccess").replace("{PLAYER}", target.playername));
         } else if (args[1].contains("s")) {
             LocalDateTime date = LocalDateTime.now().plusSeconds(Integer.parseInt(args[1].replace("s", "")));
             LocalDateTime timedate = LocalDateTime.now();
@@ -79,7 +79,7 @@ public class MuteAskHandler implements NextStepHandler {
             String formattedDate = date.format(formatter);
             String time = timedate.format(formatter);
             AuthTG.loader.setMuteTime(target.uuid, formattedDate, reason, time, user.playername);
-            user.sendMessage(AuthTG.config.getString("messages.minecraft.mutesuccess").replace("{PLAYER}", target.playername));
+            user.sendMessage(AuthTG.config.getString("messages.telegram.mutesuccess").replace("{PLAYER}", target.playername));
             String message = ChatColor.translateAlternateColorCodes('&', AuthTG.config.getString("messages.minecraft.mute")).replace("{TIMEMUTE}", AuthTG.loader.getMuteTime(target.uuid)).replace("{REASON}", AuthTG.loader.getMuteReason(target.uuid)).replace("{TIME}", AuthTG.loader.getMuteTimeAdmin(target.uuid)).replace("{ADMIN}", AuthTG.loader.getMuteAdmin(target.uuid)).replace("{BR}", "\n");
             if (target.player != null) target.player.sendMessage(message);
         } else if (args[1].equals("-s")) {
@@ -87,11 +87,11 @@ public class MuteAskHandler implements NextStepHandler {
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss dd.MM.yyyy");
             String time = timedate.format(formatter);
             AuthTG.loader.setMuteTime(target.uuid, "0", reason, time, user.playername);
-            user.sendMessage(AuthTG.config.getString("messages.minecraft.mutesuccess").replace("{PLAYER}", target.playername));
+            user.sendMessage(AuthTG.config.getString("messages.telegram.mutesuccess").replace("{PLAYER}", target.playername));
             String message = ChatColor.translateAlternateColorCodes('&', AuthTG.config.getString("messages.minecraft.mute")).replace("{TIMEMUTE}", "навсегда").replace("{REASON}", AuthTG.loader.getMuteReason(target.uuid)).replace("{TIME}", AuthTG.loader.getMuteTimeAdmin(target.uuid)).replace("{ADMIN}", AuthTG.loader.getMuteAdmin(target.uuid)).replace("{BR}", "\n");
             if (target.player != null) target.player.sendMessage(message);
         } else {
-            user.sendMessage(AuthTG.config.getString("messages.minecraft.mutetimeformat"));
+            user.sendMessage(AuthTG.config.getString("messages.telegram.mutetimeformat"));
             AuthTG.bot.remNextStepHandler(update.getMessage().getChatId());
             return;
         }
